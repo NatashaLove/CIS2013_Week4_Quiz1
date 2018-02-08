a2348faa507164d67b9b9f2a52526eb723770a30
Natasha S Love <debora@inbox.ru>
2018-02-07 18:53:04 -0600

    foo amount and bar amount

--- CIS2013_Week4_Quiz1.cpp
+++ CIS2013_Week4_Quiz1.cpp
@@ -5,6 +5,8 @@ using namespace std;
 int main (){
 
 int count = 0;
+int foo_amount = 0;
+int bar_amount = 0;
 
 
 for (int i=1; i<=2000; i++){
@@ -14,15 +16,18 @@ cout << count;
 
 if (count%50==0) {
 	cout << " FOO ";
+	foo_amount++;
 }
 if (count%33==0) {
 	cout << " BAR ";
+	bar_amount++;
 }
 cout << endl;
 
 }
 
-
+cout << " FOO amount is "<< foo_amount<< endl;
+cout << " BAR amount is "<< bar_amount << endl;
 
 return 0;
 
